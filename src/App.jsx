4d4f30b2f5ebe541446e2fff671bca3951d20b72
Ajmal <noorudheenajmal@gmail.com>
2023-12-18 14:59:08 +0530

    completed navBar Top

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,22 @@ function App() {
   return (
     <>
     <div className='w-[100%] h-[100vh]  bg-[#e3f1fe]'>
-     <div className='w-full h-[80px] flex justify-between bg-black'>
-        <div className='logo w-[15%]'>
-
+     <div className='w-full h-[80px] flex justify-between bg-[#e3f1fe]'>
+        <div className='logo w-[15%] flex justify-start items-center'>
+          <img src="/src/assets/imgLogo.png" className='w-fit h-full ml-3' alt="" />
         </div>
-        <div className='h-full w-[18%] flex gap-2 items-center bg-red-600'>
-            <div className='w-9/12 h-7 rounded bg-white flex items-center justify-around'>
+        <div className='h-full w-[18%] flex gap-2 items-center bg-[#e3f1fe]'>
+            <div className='w-9/12 h-7 rounded bg-white  flex items-center justify-around'>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTED877WUQU4h6NHyFKopaMfRXZeTqonL144Q&usqp=CAU" className='h-full py-0.5' alt="" />
                 <small className='font-bold text-slate-800 pr-2'>XYZ Enterprises Pvt.Ltd</small>
             </div>
-            <div className='bg-white h-7 rounded w-7'>
-                <p>v</p>
+            <div className='bg-white h-7 flex justify-center items-center rounded w-7'>
+               <KeyboardArrowDownIcon/>
             </div>
         </div>
+     </div>
+     <div>
+      
      </div>
     </div>
     </>
